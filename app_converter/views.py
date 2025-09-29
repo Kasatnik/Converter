1cf9f19218e9b5f3967d6f3d8bd6d4510e24f754
Roma <roman.kasatnik@gmail.com>
2025-09-29 18:00:18 +0300

    .

--- app_converter/views.py
+++ app_converter/views.py
@@ -1,4 +1,18 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 
 def draw_home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        print(request.FILES)
+        print(request.POST)
+        fs = FileSystemStorage()
+        print(request.FILES["file"].name)
+        fs.save(request.FILES["file"].name, request.FILES["file"])
+
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+
+    print(f"IP адрес пользователя: {ip_address}")
+
+    return render(request, "home.html", {
+        'ip': ip_address})
+
